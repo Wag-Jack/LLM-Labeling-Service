2577b6e12b946fb7616ef48dd336c2dc4f0b5244
Jack Wagner <wagjack@umich.edu>
2026-01-23 13:39:31 -0500

    Uploaded tests for ChatGPT to detect emotions and transcribing information about dialects from audio

--- transcribe_test.py
+++ transcribe_test.py
@@ -1,18 +1,27 @@
+from dotenv import load_dotenv
 from openai import OpenAI
+import os
+from pathlib import Path
+
+load_dotenv()
 
 # Initiate OpenAI client
-client = OpenAI()
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-# Transcribe audio file with Whisper model
-transcript = client.audio.transcriptions.create(
-    file=open("audio.mp3","rb"),
-    model="whisper-1"
-)
+interspeech_files = Path('Data/Interspeech2020-Accented-English-Speech-Recognition-Competition-Data')
+interspeech_wavs = list(interspeech_files.rglob('*.wav'))
 
-# Infer about the input
-analysis = client.chat.completions.create(
-    model="gpt-4-turbo",
-    input=f"Determine what accent is being spoken in the following transcript: {transcript.text}"
-)
+for wav in interspeech_wavs:
+    # Transcribe audio file with Whisper model
+    transcript = client.audio.transcriptions.create(
+        file=open(wav, "rb"),
+        model="whisper-1"
+    )
 
-print(analysis.choices[0].message.content)
+    # Infer about the input
+    analysis = client.responses.create(
+        model="gpt-4o-mini",
+        input=f"Determine what accent is being spoken in the following transcript: {transcript.text}"
+    )
+
+    print(analysis.output_text)
