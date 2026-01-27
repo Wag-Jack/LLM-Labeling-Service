347570ca65825be0e3f83635e10ab8d8b7c42af5
Jack Wagner <wagjack@umich.edu>
2026-01-27 01:29:37 -0500

    Transcription invocations done with AssemblyAI

--- service_invocations/invoke_speech_recognition.py
+++ service_invocations/invoke_speech_recognition.py
@@ -4,5 +4,12 @@ from service_invocations.speech_recognition.google_cloud import run_gc_stt
 
 def run_speech_recognition(edacc_df):
     # Run EdAcc samples through each speech recognition service
-    gc_stt = run_gc_stt(edacc_df)
+    
+    # Google Cloud Speech-to-Text
+    #gc_stt = run_gc_stt(edacc_df)
 
+    # AWS Transcribe
+    #aws_stt = run_aws_transcribe(edacc_df)
+
+    # AssemblyAI Speech-to-Text
+    aai_stt = run_assemblyai(edacc_df)
--- service_invocations/speech_recognition/assemblyai.py
+++ service_invocations/speech_recognition/assemblyai.py
@@ -1,2 +1,41 @@
-def run_assemblyai():
-    pass
+import assemblyai as aai
+from dotenv import load_dotenv
+import os
+import pandas as pd
+from pathlib import Path
+
+load_dotenv()
+
+def run_assemblyai(edacc_data):
+    # Establish connection with AsssemblyAI API
+    aai.settings.api_key = os.getenv("ASSEMBLYAI_API_KEY")
+
+    # Data dictionary to hold results
+    data = {
+        "id": [],
+        "wav_file": [],
+        "service_output": [],
+    }
+
+    for _, row in edacc_data.iterrows():
+        # Run the service on the selected file
+        audio_file = row["audio"]
+        print(audio_file)
+
+        # Upload the audio file to AssemblyAI
+        transcriber = aai.Transcriber()
+        transcript = transcriber.transcribe(audio_file)
+
+        data["id"].append(f"aa_stt_{row['id']:04d}")
+        data["wav_file"].append(row["audio"])
+        print(transcript.text, end='\n\n')
+
+        data["service_output"].append(transcript.text)
+
+    # Add in blank column for LLM judge score
+    data["llm_judge_score"] = [0.0 for r in data["id"]]
+
+    # Convert into DataFrame and save to CSV
+    aa_stt_df = pd.DataFrame(data)
+    aa_stt_df.to_csv("service_invocations/results/aa_stt.csv", index=False)
+    return aa_stt_df
